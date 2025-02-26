a6d563170487895a8643125c6f5e1f50d03ab421
mathieuforestier <mathieuforestier56@gmail.com>
2025-02-26 12:37:17 +0100

    Add files via upload

--- code_pince/code_pince.ino
+++ code_pince/code_pince.ino
@@ -1,21 +1,20 @@
 #include <Servo.h> 
+
 Servo pinceServo; // Déclaration du servo qui contrôle la pince 
 int potPin = A0; // Pin où est connecté le potentiomètre
 int valPot; // Variable pour stocker la valeur lue du potentiomètre 
 int anglePince; // Variable pour convertir la valeur du potentiomètre en angle pour le servo 
 int pinServo = 9; // Pin où est connecté le servo 
 
-void setup() { // Attacher le servo au pin 
-  pinceServo.attach(pinServo); // Initialiser le servo à une position neutre 
-  pinceServo.write(90); // Position de départ (fermé ou ouvert selon le type de pince)
-  delay(1000); // Attendre un peu pour stabiliser } 
+void setup() { 
+  pinceServo.attach(pinServo); // Attacher le servo au pin 
+  pinceServo.write(90); // Initialiser le servo à une position neutre 
+  delay(1000); // Attendre un peu pour stabiliser 
+} // La parenthèse fermante manquait ici pour la fonction setup()
 
-
-
-void loop() { // Lire la valeur du potentiomètre (entre 0 et 1023) 
-    valPot = analogRead(potPin); // Mapper la valeur lue pour obtenir un angle de 0 à 180 
-    anglePince = map(valPot, 0, 1023, 10, 170); // Déplacer la pince à l'angle calculé 
-    pinceServo.write(anglePince); // Attendre un peu pour permettre au servo de se déplacer
-    delay(15); } 
-  }
+void loop() { 
+  valPot = analogRead(potPin); // Lire la valeur du potentiomètre (entre 0 et 1023)
+  anglePince = map(valPot, 0, 1023, 10, 170); // Mapper la valeur lue pour obtenir un angle de 0 à 180 
+  pinceServo.write(anglePince); // Déplacer la pince à l'angle calculé
+  delay(15); // Attendre un peu pour permettre au servo de se déplacer
 }
